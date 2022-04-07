f931ef9abb133f9fbdf1962804db5ef32979fa87
Kiran Gurung <kg820532@gmail.com>
2022-04-07 13:03:16 +0100

    innit

--- prisma/seed.js
+++ prisma/seed.js
@@ -12,6 +12,46 @@ async function seed() {
     // Add the following channels with the members:
     // - classroom: everyone
     // - off-topic: bob and alice only
+
+    const alice = await prisma.user.create({
+        data: {
+            name: 'Alice'
+        }
+    })
+    const bob = await prisma.user.create({
+        data: {
+            name: 'bob'
+        }
+    })
+    const Fred = await prisma.user.create({
+        data: {
+            name: 'Fred'
+        }
+    })
+    
+    await prisma.channel.create({
+        data:{
+            name: 'classroom',
+            users:{
+                connect:[
+                    {id: alice.id},
+                    {id:bob.id},
+                    {id: Fred.id}
+                ]
+            }
+        }
+    })
+
+    await prisma.channel.create({
+        data:{
+            name: 'support',
+            users:{
+                connect:[
+                    {id: Fred.id}
+                ]
+            }
+        }
+    })
     process.exit(0);
 }
 
--- src/controllers/users.js
+++ src/controllers/users.js
@@ -3,7 +3,14 @@ const prisma = require('../utils/prisma');
 const getUsers = async(req, res) => {
     //TODO: Return a list of all users and the channels
     //they belong to
-    res.json({users: []})
+
+    const users = await prisma.findMany({
+        include : {
+            channels: true
+        }
+    })
+
+    res.json({users: users})
 }
 
 module.exports = { getUsers };
